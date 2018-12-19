ad6ff5fe2b6fe8bc667a5f6d3828729c663200ef
Zhaowei <zwfangio@hotmail.com>
2018-12-19 20:26:20 +0800

    add more test case for binary search

--- src/0704_binary_search/binary_search_test.go
+++ src/0704_binary_search/binary_search_test.go
@@ -3,11 +3,27 @@ package binarysearch
 import "testing"
 
 func TestSearch(t *testing.T) {
-	nums := []int{-1, 0, 3, 5, 9, 12}
-	target := 9
-	expected := 4
+	type arg struct {
+		nums   []int
+		target int
+	}
 
-	if res := search(nums, target); res != expected {
-		t.Errorf("expected %d, got %d", expected, res)
+	testCases := []arg{
+		arg{
+			nums:   []int{-1, 0, 3, 5, 9, 12},
+			target: 9,
+		},
+		arg{
+			nums:   []int{-1, 0, 3, 5, 9, 12},
+			target: 20,
+		},
+	}
+
+	expected := []int{4, -1}
+
+	for index, data := range testCases {
+		if res := search(data.nums, data.target); res != expected[index] {
+			t.Errorf("expected %d, got %d", expected[index], res)
+		}
 	}
 }
