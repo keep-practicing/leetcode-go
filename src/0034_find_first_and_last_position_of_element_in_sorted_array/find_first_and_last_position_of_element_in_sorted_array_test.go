4cb7c2ac94536f4bfebac1765d96fb0f56c55325
Zhaowei <zwfangio@hotmail.com>
2018-12-20 11:30:06 +0800

    find first and last position of element in sorted array

---
+++ src/0034_find_first_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array_test.go
@@ -0,0 +1,64 @@
+package findfirstandlastpositionofelementinsortedarray
+
+import (
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+func TestSearchRange(t *testing.T) {
+	type arg struct {
+		nums   []int
+		target int
+	}
+
+	testCases := []arg{
+		arg{
+			nums:   []int{5, 7, 7, 8, 8, 10},
+			target: 8,
+		},
+		arg{
+			nums:   []int{5, 7, 7, 8, 8, 10},
+			target: 6,
+		},
+		arg{
+			nums:   []int{1},
+			target: 1,
+		},
+		arg{
+			nums:   []int{},
+			target: 0,
+		},
+		arg{
+			nums:   []int{2, 2},
+			target: 2,
+		},
+		arg{
+			nums:   []int{1},
+			target: 0,
+		},
+	}
+
+	expected := [][]int{
+		{3, 4},
+		{-1, -1},
+		{0, 0},
+		{-1, -1},
+		{0, 1},
+		{-1, -1},
+	}
+
+	testFuncs := []func([]int, int) []int{
+		searchRange,
+		searchRange1,
+		searchRange2,
+	}
+
+	for _, testFunc := range testFuncs {
+		for index, testData := range testCases {
+			if res := testFunc(testData.nums, testData.target); !reflect.DeepEqual(res, expected[index]) {
+				t.Errorf("function %s, expected %v, got %v", runtime.FuncForPC(reflect.ValueOf(testFunc).Pointer()).Name(), expected[index], res)
+			}
+		}
+	}
+}
