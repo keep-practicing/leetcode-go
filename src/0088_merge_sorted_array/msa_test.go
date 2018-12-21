04255bbc76ffe582c36b17d5145438e0641264cb
Zhaowei <zwfangio@hotmail.com>
2018-12-21 18:13:33 +0800

    add more ut case for 88 merge sorted array

--- src/0088_merge_sorted_array/msa_test.go
+++ src/0088_merge_sorted_array/msa_test.go
@@ -6,16 +6,22 @@ import (
 )
 
 func TestMerge(t *testing.T) {
-	nums1 := []int{1, 2, 3, 7, 0, 0, 0}
-	m := 4
-	nums2 := []int{2, 5, 6}
-	n := 3
+	type arg struct {
+		nums1 []int
+		m     int
+		nums2 []int
+		n     int
+	}
+	testCases := []arg{
+		{nums1: []int{1, 2, 3, 7, 0, 0, 0}, m: 4, nums2: []int{2, 5, 6}, n: 3},
+		{nums1: []int{7, 8, 9, 0, 0, 0}, m: 3, nums2: []int{2, 5, 6}, n: 3},
+	}
 
-	expected := []int{1, 2, 2, 3, 5, 6, 7}
+	expected := [][]int{{1, 2, 2, 3, 5, 6, 7}, {2, 5, 6, 7, 8, 9}}
 
-	merge(nums1, m, nums2, n)
-
-	if !reflect.DeepEqual(nums1, expected) {
-		t.Errorf("expected %v, got %v", expected, nums1)
+	for index, data := range testCases {
+		if merge(data.nums1, data.m, data.nums2, data.n); !reflect.DeepEqual(expected[index], data.nums1) {
+			t.Errorf("expected %v, got %v", expected[index], data.nums1)
+		}
 	}
 }
