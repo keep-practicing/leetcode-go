90e8cc549c02f5492f176101300b2cdd4e1916c2
Zhaowei <zwfangio@hotmail.com>
2018-12-21 18:17:55 +0800

    fix ut case of 88 merge sorted array

--- src/0088_merge_sorted_array/msa_test.go
+++ src/0088_merge_sorted_array/msa_test.go
@@ -14,7 +14,7 @@ func TestMerge(t *testing.T) {
 	}
 	testCases := []arg{
 		{nums1: []int{1, 2, 3, 7, 0, 0, 0}, m: 4, nums2: []int{2, 5, 6}, n: 3},
-		{nums1: []int{7, 8, 9, 0, 0, 0}, m: 3, nums2: []int{2, 5, 6}, n: 3},
+		{nums1: []int{2, 5, 6, 0, 0, 0}, m: 3, nums2: []int{7, 8, 9}, n: 3},
 	}
 
 	expected := [][]int{{1, 2, 2, 3, 5, 6, 7}, {2, 5, 6, 7, 8, 9}}
