5a76144d3e9f6ffbc8ee259bc072e44fa27a0a56
Fang, Zhaowei <33212232+zwfang@users.noreply.github.com>
2018-12-22 19:04:56 +0800

    215 kth largest element in an array (#14)

---
+++ src/0215_kth_largest_element_in_an_array/kthleiaa.go
@@ -0,0 +1,45 @@
+/*
+215. Kth Largest Element in an Array
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+Find the kth largest element in an unsorted array.
+ Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+*/
+
+// time: 2018-12-22
+
+package kthleiaa
+
+// heap sort
+// time complexity: O(k * logn)
+// sapce complexity: O(1)
+func findKthLargest(nums []int, k int) int {
+	// heapify
+	for i := (len(nums) - 1) / 2; i >= 0; i-- {
+		shiftDown(nums, len(nums), i)
+	}
+
+	for i := len(nums) - 1; i >= len(nums)-k; i-- {
+		nums[i], nums[0] = nums[0], nums[i]
+		shiftDown(nums, i, 0)
+	}
+	return nums[len(nums)-k]
+}
+
+func shiftDown(nums []int, n int, i int) {
+	for 2*i+1 < n {
+		j := 2*i + 1
+		if j+1 < n && nums[j+1] > nums[j] {
+			j++
+		}
+
+		if nums[i] >= nums[j] {
+			break
+		}
+		nums[j], nums[i] = nums[i], nums[j]
+		i = j
+	}
+}
