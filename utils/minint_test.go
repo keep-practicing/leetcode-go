85a50e5a9d0609b9750fec55c3490093e4fd5715
Zhaowei <zwfangio@hotmail.com>
2018-12-27 07:47:27 +0800

    add more ut case for minint.go

--- utils/minint_test.go
+++ utils/minint_test.go
@@ -6,8 +6,9 @@ func TestCalcMinInt(t *testing.T) {
 	testData := [][]int{
 		{3, 4, 67, 8},
 		{},
+		{54, 3, 12, 1},
 	}
-	expectedData := []int{3, 0}
+	expectedData := []int{3, 0, 1}
 
 	for index, data := range testData {
 		if res := CalcMinInt(data...); res != expectedData[index] {
