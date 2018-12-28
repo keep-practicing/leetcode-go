5aa5876ccdf509486c0c4ba07b4387bc01253089
Zhaowei <zwfangio@hotmail.com>
2018-12-28 18:07:07 +0800

    add more ut case for 0122 best time to buy and sell stock 2.

--- src/0122_best_time_to_buy_and_sell_stock_2/maxprofit_test.go
+++ src/0122_best_time_to_buy_and_sell_stock_2/maxprofit_test.go
@@ -7,8 +7,9 @@ func TestMaxProfit(t *testing.T) {
 		{7, 1, 5, 3, 6, 4},
 		{1, 2, 3, 4, 5},
 		{7, 6, 4, 3, 1},
+		{}, {1},
 	}
-	expected := []int{7, 4, 0}
+	expected := []int{7, 4, 0, 0, 0}
 
 	for index, data := range testCases {
 		if res := maxProfit(data); res != expected[index] {
