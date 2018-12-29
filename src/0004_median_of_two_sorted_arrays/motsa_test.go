55b3bcb4ba2f2a9bdcc09d6818b14765dc157cd8
Zhaowei <zwfangio@hotmail.com>
2018-12-29 12:13:44 +0800

    4 solved.

---
+++ src/0004_median_of_two_sorted_arrays/motsa_test.go
@@ -0,0 +1,35 @@
+package motsa
+
+import (
+	"testing"
+)
+
+func TestFindMedianSortedArrays(t *testing.T) {
+	type arg struct {
+		nums1 []int
+		nums2 []int
+	}
+
+	testCases := []arg{
+		{nums1: []int{1, 3}, nums2: []int{2}},
+		{nums1: []int{1, 2}, nums2: []int{3, 4}},
+		{nums1: []int{}, nums2: []int{3, 4, 5}},
+		{nums1: []int{4, 5, 6}, nums2: []int{}},
+	}
+
+	expected := []float64{2, 2.5, 4, 5}
+
+	for index, data := range testCases {
+		if res := findMedianSortedArrays(data.nums1, data.nums2); res != expected[index] {
+			t.Errorf("expected %f, got %f", expected[index], res)
+		}
+	}
+
+	defer func() {
+		if err := recover(); err == nil || err != "nums1 and nums2 cannot be both empty." {
+			t.Errorf("expected err: nums1 and nums2 cannot be both empty.")
+		}
+	}()
+	findMedianSortedArrays([]int{}, []int{})
+
+}
