1133d7dd93409a46f42a703283493eb9d6e1486a
Zhaowei <33212232+zwfang@users.noreply.github.com>
2019-01-07 10:34:39 +0800

    153. Find Minimum in Rotated Sorted Array (#49)
    * 153 solved.
    
    * add 153 question link

---
+++ src/0153_find_minimum_in_rotated_sorted_array/fmirsa.go
@@ -0,0 +1,35 @@
+/*
+153. Find Minimum in Rotated Sorted Array
+https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+*/
+// time: 2019-01-07
+
+package fmirsa
+
+// binary search
+// time complexity: O( log n)
+// space complexity: O(1)
+func findMin(nums []int) int {
+	var (
+		low  int
+		high = len(nums) - 1
+		mid  int
+	)
+	for low < high {
+		mid = low + (high-low)>>1
+		if nums[high] < nums[mid] {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return nums[low]
+}
