d858cb54cc821a82e9f008123da7560d37ef9334
Fang, Zhaowei <zwfangio@hotmail.com>
2019-01-29 10:32:50 +0800

    add more ut case for 307

--- src/0307_Range_Sum_Query_Mutable/range_sum_query_mut_test.go
+++ src/0307_Range_Sum_Query_Mutable/range_sum_query_mut_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestNumArray(t *testing.T) {
 	nums := []int{1, 2, 3, 6, 7, 8, 9, 3, 4, 2, 5}
 	na := Constructor(nums)
-	if res := na.SumRange(0, 8); res != 43 {
+	if res := na.SumRange(4, 9); res != 33 {
 		t.Errorf("expected %d, got %d", 43, res)
 	}
 	na.Update(6, 10)
-	if res := na.SumRange(0, 8); res != 44 {
+	if res := na.SumRange(4, 9); res != 34 {
 		t.Errorf("expected %d, got %d", 44, res)
 	}
 }
