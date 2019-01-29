c83a6438a95c77743240cb0941575e26b12778bc
Fang, Zhaowei <zwfangio@hotmail.com>
2019-01-29 10:28:20 +0800

    add more ut case

--- src/0307_Range_Sum_Query_Mutable/range_sum_query_mut_test.go
+++ src/0307_Range_Sum_Query_Mutable/range_sum_query_mut_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestNumArray(t *testing.T) {
 	nums := []int{1, 2, 3, 6, 7, 8, 9, 3, 4, 2, 5}
 	na := Constructor(nums)
-	if res := na.SumRange(0, 10); res != 50 {
-		t.Errorf("expected %d, got %d", 50, res)
+	if res := na.SumRange(0, 8); res != 43 {
+		t.Errorf("expected %d, got %d", 43, res)
 	}
 	na.Update(6, 10)
-	if res := na.SumRange(0, 10); res != 51 {
-		t.Errorf("expected %d, got %d", 51, res)
+	if res := na.SumRange(0, 8); res != 44 {
+		t.Errorf("expected %d, got %d", 44, res)
 	}
 }
