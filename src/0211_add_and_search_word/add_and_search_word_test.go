81b76d3cf52293ff6332224a2a8d706d3ddeddaf
Fang, Zhaowei <zwfangio@hotmail.com>
2019-01-31 18:34:55 +0800

    add more ut case for 211🙏

--- src/0211_add_and_search_word/add_and_search_word_test.go
+++ src/0211_add_and_search_word/add_and_search_word_test.go
@@ -7,7 +7,7 @@ func TestAddAndSearchWord(t *testing.T) {
 	obj.AddWord("bad")
 	obj.AddWord("dad")
 	obj.AddWord("mad")
-	for i, j := range map[string]bool{"pad": false, "bad": true, ".ad": true, "b..": true} {
+	for i, j := range map[string]bool{"pad": false, "bad": true, ".ad": true, "b..": true, "b..d": false} {
 		if res := obj.Search(i); res != j {
 			t.Errorf("expected %t, got %t", j, res)
 		}
