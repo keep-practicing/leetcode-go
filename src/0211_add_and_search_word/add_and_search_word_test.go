527265de7af98a48ca651a746f8b0d9bdcda154e
Fang, Zhaowei <zwfangio@hotmail.com>
2019-01-31 18:27:20 +0800

    211 solved💥.

---
+++ src/0211_add_and_search_word/add_and_search_word_test.go
@@ -0,0 +1,15 @@
+package aasw
+
+import "testing"
+
+func TestAddAndSearchWord(t *testing.T) {
+	obj := Constructor()
+	obj.AddWord("bad")
+	obj.AddWord("dad")
+	obj.AddWord("mad")
+	for i, j := range map[string]bool{"pad": false, "bad": true, ".ad": true, "b..": true} {
+		if res := obj.Search(i); res != j {
+			t.Errorf("expected %t, got %t", j, res)
+		}
+	}
+}
