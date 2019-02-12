2a854187862d7035e41e0962596883a44f2a9ca5
Fang, Zhaowei <zwfangio@hotmail.com>
2019-02-12 09:54:41 +0800

    clean code

--- src/0211_add_and_search_word/add_and_search_word_test.go
+++ src/0211_add_and_search_word/add_and_search_word_test.go
@@ -4,9 +4,11 @@ import "testing"
 
 func TestAddAndSearchWord(t *testing.T) {
 	obj := Constructor()
-	obj.AddWord("bad")
-	obj.AddWord("dad")
-	obj.AddWord("mad")
+
+	for _, word := range []string{"bad", "dad", "mad"}{
+		obj.AddWord(word)
+	}
+
 	for i, j := range map[string]bool{"pad": false, "bad": true, ".ad": true, "b..": true, "b..d": false} {
 		if res := obj.Search(i); res != j {
 			t.Errorf("expected %t, got %t", j, res)
