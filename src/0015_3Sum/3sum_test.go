c079f77ed47710247e502620d2de20d89f9d0100
Fang, Zhaowei <zwfangio@hotmail.com>
2019-03-01 12:23:16 +0800

    fix bug

--- src/0015_3Sum/3sum_test.go
+++ src/0015_3Sum/3sum_test.go
@@ -7,14 +7,16 @@ import (
 
 func TestThreeSum(t *testing.T) {
 	testData := [][]int{
-		{-1, 0, 1, 2, -1, -4},
+		{-1, 0, 1, 2, -6, -4},
+		{-7, 3, 4, 5, 6, 6, 4},
 	}
 	expected := [][][]int{
-		{{-1, 0, 1}, {-1, -1, 2}},
+		{{-1, 0, 1}},
+		{{-7, 3, 4}},
 	}
 
 	for index, nums := range testData {
-		if res := threeSum(nums); reflect.DeepEqual(res, expected[index]) {
+		if res := threeSum(nums); !reflect.DeepEqual(res, expected[index]) {
 			t.Errorf("expected %v, got %v", expected[index], res)
 		}
 	}
