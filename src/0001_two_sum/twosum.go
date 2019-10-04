1f0138b33cc906894313f948c48bf2ff0f67fc44
zwfang <zwfangio@gmail.com>
2019-10-04 10:15:55 +0800

    go fmt

--- src/0001_two_sum/twosum.go
+++ src/0001_two_sum/twosum.go
@@ -36,10 +36,10 @@ func twoSum(nums []int, target int) []int {
 // brute force
 // Time complexity: O(n^2)
 // Space complexity: O(1)
-func twoSum1(nums []int, target int) []int{
+func twoSum1(nums []int, target int) []int {
 	for i, j := range nums {
-		for k := i+1; k < len(nums); k++ {
-			if nums[k] + j == target {
+		for k := i + 1; k < len(nums); k++ {
+			if nums[k]+j == target {
 				return []int{i, k}
 			}
 		}
--- src/0001_two_sum/twosum_test.go
+++ src/0001_two_sum/twosum_test.go
@@ -8,13 +8,13 @@ import (
 func TestTwoSum(t *testing.T) {
 	nums := []int{2, 7, 11, 15}
 
-	funcs := []func ([]int, int) []int {twoSum, twoSum1}
+	funcs := []func([]int, int) []int{twoSum, twoSum1}
 
 	for _, testFunc := range funcs {
 		if res := testFunc(nums, 9); !reflect.DeepEqual(res, []int{0, 1}) {
 			t.Error("Failed, two sum")
 		}
-	
+
 		if res := testFunc(nums, 6); !reflect.DeepEqual(res, []int{}) {
 			t.Error("Failed, two sum")
 		}
